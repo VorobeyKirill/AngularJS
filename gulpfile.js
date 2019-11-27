e62d7db2e130bb9c7e9d758152712fb1b2c7dcf5
VorobeyKirill <vorobeykirill@mail.ru>
2019-11-27 12:29:16 +0300

    feat: add gulp-sass plugin and global css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+sass.compiler = require('node-sass');
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -7,4 +10,15 @@ gulp.task('serve', function() {
     });
 
     gulp.watch('./index.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concat('main.css'))
+      .pipe(gulp.dest('./src'));
+  });
+   
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
